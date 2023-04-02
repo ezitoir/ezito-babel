413214be93bbdbdbb499f348bbde685956b2c2ea
ezito developer <33319787+ezitoir@users.noreply.github.com>
2023-04-02 05:54:05 -0700

    Add files via upload

--- utils/function-name-creator.js
+++ utils/function-name-creator.js
@@ -1,4 +1,4 @@
-
+ 
 
 module.exports = function functionNameCreator(functionNameList , templateKey = 'ARGUMENTS'){
     functionNameList = functionNameList instanceof Array ? functionNameList : [ functionNameList ];
